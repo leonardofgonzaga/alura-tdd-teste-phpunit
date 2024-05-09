766b62016d871892beaa9c3b8d54b318cddb52b4
Leonardo Gonzaga <leonardo.fg95@gmail.com>
2024-05-09 06:51:49 -0300

    Aula 3: classes de equivalência

--- src/Service/Avaliador.php
+++ src/Service/Avaliador.php
@@ -2,17 +2,19 @@
 
 namespace Alura\Leilao\Service;
 
+use Alura\Leilao\Model\Lance;
 use Alura\Leilao\Model\Leilao;
 
 class Avaliador
 {
     private $maiorValor = -INF;
     private $menorValor = INF;
+    private $maioresLances;
 
     public function avalia(Leilao $leilao): void
     {
         foreach ($leilao->getLances() as $lance) {
-            
+
             if ($lance->getValor() > $this->maiorValor) {
                 $this->maiorValor = $lance->getValor();
             }
@@ -21,6 +23,13 @@ class Avaliador
                 $this->menorValor = $lance->getValor();
             }
         }
+
+        $lances = $leilao->getLances();
+        usort($lances, function(Lance $lance1, Lance $lance2) {
+            return $lance2->getValor() - $lance1->getValor();
+        });
+
+        $this->maioresLances = array_slice($lances, 0, 3);
     }
 
     public function getMaiorValor(): float
@@ -31,5 +40,13 @@ class Avaliador
     public function getMenorValor(): float
     {
         return $this->menorValor;
+    }
+
+    /** 
+     * @return Lance[]
+    */
+    public function getMaioresLances(): array
+    {
+        return $this->maioresLances;
     }
 }
